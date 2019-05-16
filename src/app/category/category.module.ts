dbc21308af3e7409d20cb30efb6c89fb3e2ee509
Roman Lapshin <bo83@mail.ru>
2019-05-16 09:35:23 +0300

    Без REST

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { CategoryRoutingModule } from './category-routing.module';
-import { ListModule } from './list/list.module';
-import { EditModule } from './edit/edit.module';
+import { NgModule } from "@angular/core";
+import { CategoryRoutingModule } from "./category-routing.module";
+import { ListModule } from "./list/list.module";
+import { EditModule } from "./edit/edit.module";
 
 @NgModule({
   imports: [
--- src/app/category/edit/edit.component.ts
+++ src/app/category/edit/edit.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Category } from "../category.model";
+import { CategoryFormGroup } from "./form.model";
 
 @Component({
     selector: 'edit-app',
     templateUrl: './edit.component.html'
 })
 
-export class EditComponent { }
+export class EditComponent { 
+
+    newCategory: Category = new Category();
+    formGroup: CategoryFormGroup = new CategoryFormGroup();
+    formSubmitted: boolean = false;
+
+    submitForm(form: NgForm) {
+        this.formSubmitted = true;
+        if (this.formGroup.valid) {
+            this.newCategory.name = form.value.name;
+        }
+    }
+
+    /*private addCategory() {
+        this.rest.addCategory(this.newCategory).subscribe((result) => {
+            console.log(result);
+          }, (err) => {
+            console.log(err);
+          });
+    }*/
+}
